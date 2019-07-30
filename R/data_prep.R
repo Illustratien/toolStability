c8edf161edb3f15ef0e5bf957bec91f6ae3a7441
TC <tien.wang@gem.uni-hannover.de>
2019-07-30 09:45:28 +0200

    improve efficiency by removing %>%

--- R/data_prep.R
+++ R/data_prep.R
@@ -14,7 +14,7 @@
 #' @param Genotype genetic varieties
 #' @param Environment environmental parameters
 #' @export
-#' @importFrom magrittr %>%
+#' @importFrom dplyr group_by summarise
 #' @importFrom stats sd var
 #' @importFrom data.table data.table
 #' @return a dataframe with descriptive statics
@@ -29,29 +29,28 @@ data_prep <- function(data,trait,Genotype,Environment){
 # use 'X' as abbreviation for simplyfying purpose
 # in the latter equation for stability indices calculation
   names(data)[names(data) == trait] <- 'X'
-  data <- data%>%data.table(key='Genotype,Environment,X')
+  data <- data.table(data,key='Genotype,X,Environment')
   # Genotypic mean, sd, cv, log 10 of variance and mean
-  GM <- data%>%
-    dplyr::group_by(Genotype)%>%
-    dplyr::summarise(
-      Xi.bar=mean(data$X),
-      Xi.sd=sd(data$X),
-      Xi.cv=sd(data$X)/mean(data$X),
-      Xi.logvar=log10(var(data$X)),
-      Xi.logmean=log10(mean(data$X)))%>%
-    data.table(key='Genotype')
+  GM <- data.table(
+    summarise(
+      group_by(data,Genotype),
+      Xi.bar=mean(X),
+      Xi.sd=sd(X),
+      Xi.cv=sd(X)/mean(X),
+      Xi.logvar=log10(var(X)),
+      Xi.logmean=log10(mean(X))),
+    key='Genotype')
 
   # Environmental mean, Xj
-  EM<- data%>%
-    dplyr::group_by(Environment)%>%
-    dplyr::summarise(
-      Xj.bar=mean(data$X),
-      Xj.max=max(data$X))%>%
-    data.table(key='Environment')
+  EM <- data.table(
+    summarise(
+      group_by(data,Environment),
+      Xj.bar=mean(X),Xj.max=max(X)),
+    key='Environment')
 
   # Combine genotypic and environmental data
-  data <- merge(data, GM, by="Genotype")
-  data <- merge(data, EM, by="Environment")
+  data <- inner_join(data, GM, by="Genotype")
+  data <- inner_join(data, EM, by="Environment")
 
 return(data)
 }
